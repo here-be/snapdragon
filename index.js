a487be51afe1544f9f8c46f3eb0b768ecbb04c74
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-31 02:34:40 -0400

    simplify, formatting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var extend = require('extend-shallow');
-var Render = require('./lib/render');
-var Parse = require('./lib/parse');
+var Renderer = require('./lib/renderer');
+var Parser = require('./lib/parser');
 
 /**
  * Initialize `Snapdragon` with the given `string` and `options`.
@@ -13,87 +13,48 @@ var Parse = require('./lib/parse');
  */
 
 function Snapdragon(str, options) {
+  if (!(this instanceof Snapdragon)) {
+    return new Snapdragon(str, options);
+  }
   this.options = options || {};
   this.input = str;
-  this.transforms = {};
 }
 
-/**
- * Transforms are used to extend or modify the `this` object
- * upon initialization.
- *
- * @param {String} `name` The name of the transform to add.
- * @param {Function} `fn` The actual transform function.
- * @return {Object} Returns `Snapdragon` for chaining.
- * @api public
- */
+Snapdragon.prototype = {
+  constructor: Snapdragon,
 
-Snapdragon.prototype.transform = function(name, fn) {
-  if (typeof name !== 'string') {
-    throw new TypeError('Snapdragon#transform expects `name` to be a string.');
-  }
-  if (arguments.length === 1) {
-    return this.transforms[name];
-  }
-  if (fn && typeof fn === 'function') {
-    this.transforms[name] = fn;
-    fn.call(this, this);
-  }
-  return this;
-};
+  /**
+   * Parse the given string into an ast by calling
+   * each parser in the middleware stack.
+   *
+   * @param  {String} `str` String to parse
+   * @param  {Object} `options`
+   * @return {Object} Returns a parsed ast
+   * @api public
+   */
 
-/**
- * Add a method to the `Snapdragon` prototype.
- *
- * @param  {string} `name`
- * @param  {Function} `fn`
- */
+  parser: function(str, options) {
+    if (typeof str !== 'string') {
+      options = str;
+      str = this.input;
+    }
+    var opts = extend({}, this.options, options);
+    return new Parser(str, opts);
+  },
 
-Snapdragon.prototype.mixin = function(name, fn) {
-  if (typeof name !== 'string') {
-    throw new TypeError('Snapdragon#mixin expects `name` to be a string.');
+  /**
+   * Render a string by visiting over an array of ast nodes.
+   *
+   * @param  {Object} `ast`
+   * @param  {Object} `options`
+   * @return {String} Rendered string.
+   * @api public
+   */
+
+  renderer: function(ast, options) {
+    var opts = extend({}, this.options, options);
+    return new Renderer(ast, options);
   }
-  if (name && fn && typeof fn === 'function') {
-    Snapdragon.prototype[name] = fn;
-  }
-  return this;
-};
-
-/**
- * Parse the given string into an ast by calling
- * each parser in the middleware stack.
- *
- * @param  {String} `str` String to parse
- * @param  {Object} `options`
- * @return {Object} Returns a parsed ast
- * @api public
- */
-
-Snapdragon.prototype.parser = function(str, options) {
-  if (str && typeof str === 'object') {
-    options = str;
-    str = null;
-  }
-  var opts = extend({}, this.options, options);
-  this.input = str || this.input;
-  if (typeof this.input === 'undefined') {
-    throw new TypeError('Snapdragon#parse expects a string');
-  }
-  return new Parse(this.input, opts);
-};
-
-/**
- * Render a string by visiting over an array of ast nodes.
- *
- * @param  {Object} `ast`
- * @param  {Object} `options`
- * @return {String} Rendered string.
- * @api public
- */
-
-Snapdragon.prototype.renderer = function(ast, options) {
-  var opts = extend({}, this.options, options);
-  return new Render(ast, options);
 };
 
 /**
@@ -106,5 +67,5 @@ module.exports = Snapdragon;
  * Expose `Parse` and `Render`
  */
 
-module.exports.Parser = Parse;
-module.exports.Renderer = Render;
+module.exports.Parser = Parser;
+module.exports.Renderer = Renderer;
