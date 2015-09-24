4b47b8535fa6398f81b776447e80cda0adc7d8aa
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-23 22:26:09 -0400

    adds gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var stylish = require('jshint-stylish');
+var istanbul = require('gulp-istanbul');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var gulp = require('gulp');
+
+var lint = ['index.js', 'lib/*.js'];
+
+gulp.task('lint', function () {
+  return gulp.src(lint)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('coverage', function () {
+  return gulp.src(lint)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['coverage'], function (cb) {
+  return gulp.src('test/*.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports({
+      reporters: [ 'text' ],
+      reportOpts: {dir: 'coverage', file: 'summary.txt'}
+    }))
+});
+
+gulp.task('default', ['lint', 'test']);
