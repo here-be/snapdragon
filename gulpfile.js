18498a9f36850fbe72b678c3c4cfffcb4e0747fc
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-27 00:06:25 -0400

    adds gulp-unused for linting deps

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
+var unused = require('gulp-unused');
 
 gulp.task('coverage', function() {
   return gulp.src(['lib/*.js', 'index.js'])
@@ -21,6 +22,11 @@ gulp.task('eslint', function() {
   return gulp.src(['*.js', 'lib/*.js', 'test/*.js'])
     .pipe(eslint())
     .pipe(eslint.format());
+});
+
+gulp.task('unused', function() {
+  return gulp.src(['index.js', 'lib/*.js'])
+    .pipe(unused({keys: Object.keys(require('./lib/utils.js'))}));
 });
 
 gulp.task('default', ['coverage', 'eslint', 'mocha']);
